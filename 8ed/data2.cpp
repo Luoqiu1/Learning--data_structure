d557493ec3894f11c3aa8b713ffe31779dc55893
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-10 23:36:37 +0800

    调试

--- 8ed/data2.cpp
+++ 8ed/data2.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-//using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 #define True 1
 #define Error 1
 #define Overflow -2
@@ -7,32 +7,31 @@
 #define STACK_INIT_SIZE 100
 #define STACKINCREMENT 10
 typedef int Status;
-typedef int SElemType;
+typedef char SElemType;
 typedef struct {
 	SElemType *top,*base;
 	int stacksize;
 }SqStack;
 
-Status InitStack(SqStack &S)
-{
-	S.base=(SElemType*)malloc(sizeof(SElemType)*STACK_INIT_SIZE);
-	if(!S.base)exit(Overflow);
-	S.top=S.base;
-	S.stacksize=STACK_INIT_SIZE;
-	return Ok;
-}
 
-Status pop(SqStack &S)
+
+Status pop(SqStack &S,SElemType &e)
 {
 	if(S.base==S.top)exit(Overflow);
+	e=*S.top;
 	--S.top;
 	return Ok;
 }
 
+Status StackEmpty(SqStack S)
+{
+	if(S.base==S.top)return Ok;
+	return !Ok;
+}
+
 Status push(SqStack &S,SElemType e)
 {
-	if(e=='#')pop(S);
-	else if(S.base-S.top>=S.stacksize){
+	if(S.base-S.top>=S.stacksize){
 		S.base=(SElemType*)realloc(S.base,(S.stacksize+STACKINCREMENT)*sizeof(SElemType));
 		if(!S.base)exit(Overflow);
 		S.top=S.base+S.stacksize;
@@ -41,6 +40,26 @@ Status push(SqStack &S,SElemType e)
 	return Ok;
 }
 
+
+Status InitStack(SqStack &S)
+{
+//	S.base=(SElemType*)malloc(sizeof(SElemType)*STACK_INIT_SIZE);
+//	if(!S.base)exit(Overflow);
+//	S.top=S.base;
+//	S.stacksize=STACK_INIT_SIZE;
+//	return Ok;
+
+	char s[100];scanf("%s",s);
+	int i=0;push(S,s[i]);
+	char ch;
+	while(!StackEmpty(S)){
+		pop(S,ch);if(ch!='#')s[i]=ch;
+		else {
+			
+		}
+	}
+	
+}
 
 int main ()
 {
