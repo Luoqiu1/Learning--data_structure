db72565fb3e34a77441944a4814d4d83784edf51
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-21 20:50:51 +0800

    再看

--- 9ed/data1(new2.cpp
+++ 9ed/data1(new2.cpp
@@ -17,40 +17,57 @@ typedef struct BiThrNode{
 }BiThrNode,*BiThrTree;
 BiThrTree pre=nullptr;
 
-Status CreateBiThrTree(BiThrTree &T)
-{
-	T=(BiThrNode*)malloc(sizeof(BiThrNode));
-	if(!T)exit(Overflow);
-	char ch;scanf("%c",&ch);
-	if(ch=='#')exit(Overflow);
-	T->data=ch;T->lchild=nullptr;T->rchild=nullptr;//T->LTag=Thread;T->RTag=Thread; 
-	int FatherNum=1;
-	BiThrNode **FatherTree=(BiThrNode**)malloc(sizeof(BiThrNode*)*FatherNum);
-	FatherTree[0]=T;
-	while(FatherNum){
-		int ChildNum=FatherNum*2;
-		BiThrNode **ChildTree=(BiThrNode**)malloc(sizeof(BiThrNode*)*ChildNum);
-		int j=0;int NewChildNum=0;
-		while(ChildNum--){
-			scanf("%c",&ch);if(ch=='#')j++;
-			else{	
-				BiThrTree q=(BiThrNode*)malloc(sizeof(BiThrNode));
-				if(!q)exit(Overflow);
-				q->data=ch;q->lchild=nullptr;q->rchild=nullptr;//q->LTag=Thread;q->RTag=Thread;
-				ChildTree[NewChildNum++]=q;
-				if(j%2==0){//左树 
-					FatherTree[j/2]->lchild=q;
-				}
-				else{
-					FatherTree[j/2]->rchild=q;
-				}
-				j++;
-			}
-		}
-		FatherNum=NewChildNum;
-		free(FatherTree);
-		FatherTree=ChildTree;
-	}
+//Status CreateBiThrTree(BiThrTree &T)
+//{
+//	T=(BiThrNode*)malloc(sizeof(BiThrNode));
+//	if(!T)exit(Overflow);
+//	char ch;scanf("%c",&ch);
+//	if(ch=='#')exit(Overflow);
+//	T->data=ch;T->lchild=nullptr;T->rchild=nullptr;//T->LTag=Thread;T->RTag=Thread; 
+//	int FatherNum=1;
+//	BiThrNode **FatherTree=(BiThrNode**)malloc(sizeof(BiThrNode*)*FatherNum);
+//	FatherTree[0]=T;
+//	while(FatherNum){
+//		int ChildNum=FatherNum*2;
+//		BiThrNode **ChildTree=(BiThrNode**)malloc(sizeof(BiThrNode*)*ChildNum);
+//		int j=0;int NewChildNum=0;
+//		while(ChildNum--){
+//			scanf("%c",&ch);if(ch=='#')j++;
+//			else{	
+//				BiThrTree q=(BiThrNode*)malloc(sizeof(BiThrNode));
+//				if(!q)exit(Overflow);
+//				q->data=ch;q->lchild=nullptr;q->rchild=nullptr;//q->LTag=Thread;q->RTag=Thread;
+//				ChildTree[NewChildNum++]=q;
+//				if(j%2==0){//左树 
+//					FatherTree[j/2]->lchild=q;
+//				}
+//				else{
+//					FatherTree[j/2]->rchild=q;
+//				}
+//				j++;
+//			}
+//		}
+//		FatherNum=NewChildNum;
+//		free(FatherTree);
+//		FatherTree=ChildTree;
+//	}
+//}
+
+void CreateBiThrTree(BiThrTree * tree){
+    char data;
+    scanf("%c",&data);
+    if (data!='#'){
+        if (!((*tree)=(BiThrNode*)malloc(sizeof(BiThrNode)))){
+            printf("申请结点空间失败");
+            return;
+        }else{
+            (*tree)->data=data;//采用前序遍历方式初始化二叉树
+            CreateBiThrTree(&((*tree)->lchild));//初始化左子树
+            CreateBiThrTree(&((*tree)->rchild));//初始化右子树
+        }
+    }else{
+        *tree=NULL;
+    }
 }
 
 void InThreading(BiThrTree p)
@@ -68,33 +85,43 @@ void InThreading(BiThrTree p)
 	}
 }
 
-Status InOrderThreading(BiThrTree T,BiThrTree &Thrt)
-{
-	Thrt=(BiThrNode*)malloc(sizeof(BiThrNode));if(!Thrt)exit(Overflow);
-	Thrt->LTag=Link;Thrt->RTag=Thread;
-	Thrt->rchild=Thrt;
-	if(!T)Thrt->lchild=Thrt;
-	else{
-		Thrt->lchild=T;
-		pre=Thrt;
-		InThreading(T);
-		pre->rchild=Thrt;pre->RTag=Thread;
-		Thrt->rchild=pre;
-	}
-}
+//Status InOrderThreading(BiThrTree T,BiThrTree &Thrt)
+//{
+//	Thrt=(BiThrNode*)malloc(sizeof(BiThrNode));if(!Thrt)exit(Overflow);
+//	Thrt->LTag=Link;Thrt->RTag=Thread;
+//	Thrt->rchild=Thrt;
+//	if(!T)Thrt->lchild=Thrt;
+//	else{
+//		Thrt->lchild=T;
+//		pre=Thrt;
+//		InThreading(T);
+//		pre->rchild=Thrt;pre->RTag=Thread;
+//		Thrt->rchild=pre;
+//	}
+//}
 
 Status InOrderTraverse_Thr(BiThrTree T)
 {
-	BiThrTree p=T->lchild;
-	while(p!=T){
-		while(p->LTag==Link){
-			p=p->lchild;
+//	BiThrTree p=T->lchild;
+//	while(p!=T){
+//		while(p->LTag==Link){
+//			p=p->lchild;
+//		}
+//		cout<<p->data;
+//		while(p->RTag==Thread&&p->rchild!=T){
+//		p=p->rchild;cout<<p->data;
+//		}
+//		p=p->rchild;
+//	}
+
+	while(T){
+		while(T->LTag==Link)T=T->lchild;
+		cout<<T->data;
+		while(T->RTag==Thread&&T->rchild){
+			T=T->rchild;
+			cout<<T->data;
 		}
-		cout<<p->data;
-		while(p->RTag==Thread&&p->rchild!=T){
-		p=p->rchild;cout<<p->data;
-		}
-		p=p->rchild;
+		T=T->rchild;
 	}
 	return Ok;
 }
@@ -103,7 +130,8 @@ int main ()
 {
 	BiThrTree T,Thrt;
 	printf("层序创建二叉树，输入结点的值：\n");
-	CreateBiThrTree(T);
-	InOrderThreading(T,Thrt);
+	CreateBiThrTree(&T);
+//	InOrderThreading(T,Thrt);
+	InThreading(T);
 	InOrderTraverse_Thr(Thrt);cout<<endl;
 }
