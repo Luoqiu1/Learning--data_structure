4386ae398ab7acc37639c9267363772ffbd91634
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-04-22 19:15:50 +0800

    复习及注释

--- 9ed/data1(new2.cpp
+++ 9ed/data1(new2.cpp
@@ -55,41 +55,8 @@ q->LTag=Link;q->RTag=Link;  //这里仔细。。。！再看看为什么少这两语句会错
 	}
 }
 
-//void CreateBiThrTree(BiThrTree &T){
-//    char data;
-//    cin>>data;
-//    if(data=='#') T=NULL;
-//    else{
-//    	T=(BiThrNode *)malloc(sizeof(BiThrNode));
-//		T->data=data;
-//    	T->LTag=Link;
-//    	T->RTag=Link;
-//    	//printf("here\n");
-//    	CreateBiThrTree(T->lchild);
-//    	CreateBiThrTree(T->rchild);
-//	}
-//}
-
-//void CreateBiThrTree(BiThrTree * tree){
-//    char data;
-//    scanf("%c",&data);
-//    if (data!='#'){
-//        if (!((*tree)=(BiThrNode*)malloc(sizeof(BiThrNode)))){
-//            printf("申请结点空间失败");
-//            return;
-//        }else{
-//            (*tree)->data=data;//采用前序遍历方式初始化二叉树
-//      //      (*tree)->LTag=Link;(*tree)->RTag=Link; 
-//            CreateBiThrTree(&((*tree)->lchild));//初始化左子树
-//            CreateBiThrTree(&((*tree)->rchild));//初始化右子树
-//        }
-//    }else{
-//        *tree=NULL;
-//    }
-//}
-
 void InThreading(BiThrTree p)
-{
+{//中序线索化 
 	if(p){
 		InThreading(p->lchild);
 		if(!p->lchild){
@@ -106,56 +73,49 @@ void InThreading(BiThrTree p)
 Status InOrderThreading(BiThrTree T,BiThrTree &Thrt)
 {
 	Thrt=(BiThrNode*)malloc(sizeof(BiThrNode));if(!Thrt)exit(Overflow);
-	Thrt->LTag=Link;Thrt->RTag=Thread;
-	Thrt->rchild=Thrt;
+	Thrt->LTag=Link;  //头结点“左子树”（应该）非空 
+	Thrt->RTag=Thread;//头结点“右子树”暂空 
+	Thrt->rchild=Thrt;//回指自身，以防止在线索化时出现指向首结点（首结点是第一个有意义的结点，不是头结点） 
 	if(!T)Thrt->lchild=Thrt;
 	else{
 		Thrt->lchild=T;
 		pre=Thrt;
 		InThreading(T);
-		pre->rchild=Thrt;pre->RTag=Thread;
+		pre->rchild=Thrt;pre->RTag=Thread;//最后一个结点指向头结点 
 		Thrt->rchild=pre;
 	}
 }
 
 Status InOrderTraverse_Thr(BiThrTree T)
-{
+{//中序遍历 
 	BiThrTree p=T->lchild;
 	while(p!=T){
-		while(p->LTag==Link){
+		while(p->LTag==Link){//访问到最左下子树 
 			p=p->lchild;
 		}
-		cout<<p->data;
-		while(p->RTag==Thread&&p->rchild!=T){
-		p=p->rchild;cout<<p->data;
+		cout<<p->data;//中序输出结点 
+		while(p->RTag==Thread&&p->rchild!=T){//线索访问后继 
+		p=p->rchild;cout<<p->data;//直接输出后继 
 		}
-		p=p->rchild;
+		p=p->rchild;//1、有右子树，进入，并进入寻找下一次中序输出的第一个结点。 
+					//2、p->rchild==T，进入，结束循环 
 	}
-
-//	while(T){
-//		while(T->LTag==Link)T=T->lchild;
-//		cout<<T->data;
-//		while(T->RTag==Thread&&T->rchild){
-//			T=T->rchild;
-//			cout<<T->data;
-//		}
-//		T=T->rchild;
-//	}
 	return Ok;
 }
 
 void PreThreading(BiThrTree p)
 {
 	if(p){
-		if(!p->lchild){
+		if(!p->lchild){//左子树空，前驱结点确定 
 			p->lchild=pre;p->LTag=Thread;
 		}
-		if(!pre->rchild){
+		if(!pre->rchild){//上一个访问的结点右子树空，后继结点确定为当前结点 
 			pre->rchild=p;pre->RTag=Thread;
 		}
-		pre=p;
-		if(p->LTag==Link)PreThreading(p->lchild);
-		if(p->RTag==Link)PreThreading(p->rchild);
+		pre=p;//更新 
+		if(p->LTag==Link)PreThreading(p->lchild);//其实可以说，在某种意义上
+		if(p->RTag==Link)PreThreading(p->rchild);//p->L/R Tag==0(Link)表示其 左/右 子树非空，
+												 //Tag==1(Thread)则 子树空。利用空域去指向前驱或后继元素！ 
 	}
 }
 
@@ -179,14 +139,14 @@ Status PreOrderTraverse_Thr(BiThrTree T)
 	BiThrTree p=T->lchild;
 	while(p!=T){
 		cout<<p->data;
-		while(p->LTag==Link){
-		p=p->lchild;cout<<p->data;
-		}
+		while(p->LTag==Link){//找到最左下角 
+		p=p->lchild;cout<<p->data;//先序 每次直接访问 
+		}//循环结束表示左子树空了，要访问下一个 
 //		while(p->RTag==Thread&&p->rchild!=T){
 //			p=p->rchild;cout<<p->data;
 //		}
-	//	cout<<p->data; 
-		p=p->rchild;
+//		cout<<p->data; 
+		p=p->rchild;//不论是后继结点还是右子树均在p->rchild中。进去直接输出（先序，右子树直接输出） 
 	}
 }
 
