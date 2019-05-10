db9b7d9ed24c4e1c50d4d7d4ba42e7aa6cb17869
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-10 14:21:53 +0800

    邻接表 无向图的构建完成

--- 12ed/data2.cpp
+++ 12ed/data2.cpp
@@ -1,10 +1,9 @@
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
 #define Ok 1
 #define Error 0
 #define Overflow -2
 #define True 1
-#define INFINITY 0x3f3f3f3f
 #define MAX_VERTEX_NUM 20 
 typedef int Status;
 typedef char VertexType;
@@ -48,32 +47,41 @@ Status CreateUDG(ALGraph &G)//无向图
 	int i,j,k,Inclnfo;
 	ArcNode *pi,*pj;
 	VertexType v1,v2;
+//scanf("%d %d %d",&G.vexnum,&G.arcnum,&IncInfo);
+		//IncInfo为0表明各弧不含其他信息 
 	printf("输入顶点数 G.vexnum：");scanf("%d",&G.vexnum);
 	printf("输入边数 G.arcnum：");scanf("%d",&G.arcnum);
 	getchar(); 
 	for(i=0;i<G.vexnum;++i){
 		G.vertices[i].firstarc=nullptr;
-		printf("输入顶点 G.vertices[%d]",i);
-		scanf("%c",G.vertices[i]);getchar();
+		printf("输入顶点 G.vertices[%d].data",i);
+		scanf("%c",G.vertices[i].data);getchar();
 	}
 	for(k=0;k<G.arcnum;++k){
-		printf("输入第 %d 条边的顶点 v1、v2：\n",k+1);
-		scanf("%c %c",(*pi).data,(*pj).data);
-		i=LocateVex(G,(*pi).data);j=LocateVex(G,(*pj).data);
-		(*pi).nextarc=nullptr;(*pj).nextarc=nullptr;
-		if(!G.vertices[i].firstarc){
-			G.vertices[i].firstarc=pi;
-		}
-		else{
-			
-		}
-		if(!G.vertices[j].firstarc){
+		printf("请输入第 %d 条边的顶点 v1、v2：\n",k+1);
+	//	scanf("%c %c",(*pi).data,(*pj).data);
+		//上一行不需要这样操作！顶点的集合已经构造完成了！
+		//现在只需要找到代表 v1、v2的顶点的位置！
+		scanf("%c %c",&v1,&v2);getchar();
+		i=LocateVex(G,v1);j=LocateVex(G,v2);
+		if(!(pi=(ArcNode*)malloc(sizeof(ArcNode))))exit(Overflow);
+	//	else{   不必else，因为如果满足的话已经退出了 
+			pi->adjvex=j;pi->nextarc=G.vertices[i].firstarc;// 这两行是插入链表
+			G.vertices[i].firstarc=pi;// 后来的弧插入在链表头结点的下一个链位置！ 
+	//	}
+		if(!(pj=(ArcNode*)malloc(sizeof(ArcNode))))exit(Overflow);
+	//	else{
+			pj->adjvex=i;pj->nextarc=G.vertices[j].firstarc;
 			G.vertices[j].firstarc=pj;
-		}
-		else{
-			
-		}
+	//	}
+		/* if(IncInfo){
+				printf("请输入弧包含的相关信息情况：\n");
+				scanf("%d",pj->info);
+				pi->info=pj->info; 
+			}
+		*/		
 	}
+	return Ok; 
 }
 
 int main ()
