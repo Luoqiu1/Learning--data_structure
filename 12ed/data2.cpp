a86a1870be269b02fe3fa72b6504257fdad17f53
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-10 13:25:44 +0800

    准备开始实现链表

--- 12ed/data2.cpp
+++ 12ed/data2.cpp
@@ -15,6 +15,7 @@ typedef enum{DG,DN,UDG,UDN}GraphKind;
 typedef struct ArcNode{
 	
 	//还是忘记了，邻接表的特征啊！是在同一个顶点下的邻接的弧的集合！
+	//通过特征来理解数据结构！因为数据结构是为了满足某一要求（特征）而建立的！ 
 	int adjvex; 
 	//	
 	InfoType *info;
@@ -32,6 +33,48 @@ typedef struct ALGraph{
 	//当前情况下的顶点数及弧数 一定不要忘记啊。。。 
 	int vexnum,arcnum;
 }ALGraph;
+
+int LocateVex(ALGraph G,char v)
+{
+	int i;
+	for(i=0;i<G.vexnum;++i){
+		if(G.vertices[i].data==v)return i;
+	}
+	return -1;
+}
+
+Status CreateUDG(ALGraph &G)//无向图 
+{
+	int i,j,k,Inclnfo;
+	ArcNode *pi,*pj;
+	VertexType v1,v2;
+	printf("输入顶点数 G.vexnum：");scanf("%d",&G.vexnum);
+	printf("输入边数 G.arcnum：");scanf("%d",&G.arcnum);
+	getchar(); 
+	for(i=0;i<G.vexnum;++i){
+		G.vertices[i].firstarc=nullptr;
+		printf("输入顶点 G.vertices[%d]",i);
+		scanf("%c",G.vertices[i]);getchar();
+	}
+	for(k=0;k<G.arcnum;++k){
+		printf("输入第 %d 条边的顶点 v1、v2：\n",k+1);
+		scanf("%c %c",(*pi).data,(*pj).data);
+		i=LocateVex(G,(*pi).data);j=LocateVex(G,(*pj).data);
+		(*pi).nextarc=nullptr;(*pj).nextarc=nullptr;
+		if(!G.vertices[i].firstarc){
+			G.vertices[i].firstarc=pi;
+		}
+		else{
+			
+		}
+		if(!G.vertices[j].firstarc){
+			G.vertices[j].firstarc=pj;
+		}
+		else{
+			
+		}
+	}
+}
 
 int main ()
 {
