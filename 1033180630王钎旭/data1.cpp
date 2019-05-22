1480c463c6d7553c0b584d815d0eb65330a2b47e
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-22 20:15:02 +0800

    修改

--- 1033180630王钎旭/data1.cpp
+++ 1033180630王钎旭/data1.cpp
@@ -1,252 +1,206 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
 #define Ok 1
 #define Error 0
 #define Overflow -2
 #define True 1
-#define INFINITY 0x3f3f3f3f
-#define MAX_VERTEX_NUM 20 
+#define False 0
+#define STACK_INIT_SIZE 100
+#define STACKINCREMENT 10
+typedef int KeyType;
 typedef int Status;
-typedef char VertexType;
-typedef int InfoType;
-typedef int VRType;
-typedef enum{DG,DN,UDG,UDN}GraphKind; 
+typedef int TElemType;
+typedef struct{
+	KeyType key;//关键字域 
+}ElemType;
+typedef struct BiTNode{
+	TElemType data;
+	struct BiTNode *rchild,*lchild; 
+}BiTNode,*BiTree;
+typedef BiTree SElemType;
+typedef struct SqStack{
+	SElemType *top,*base;
+	int stacksize;
+}SqStack;
 
-typedef struct AdjCell{
-	VRType adj;
-	InfoType *info;
-}AdjCell,AdjMatrix[MAX_VERTEX_NUM][MAX_VERTEX_NUM];
-typedef struct MGraph{
-	GraphKind kind;
-	int vexnum,arcnum;
-	AdjMatrix arcs;
-	VertexType vexs[MAX_VERTEX_NUM];
-}MGraph; 
-
-int LocateVex(MGraph G,char v)//定位顶点的位置！ 
+Status StackEmpty(SqStack S)
 {
-	int i;
-	for(i=0;i<G.vexnum;++i){
-		if(G.vexs[i]==v)return i;
-	}
-	return -1;//未找到符合条件的顶点！ 
+	if(S.base==S.top)return Ok;
+	return !Ok;
 }
 
-Status CreateUDN(MGraph &G)
+Status InitStack(SqStack &S)
 {
-	int i,j,k,w;
-	VertexType v1,v2;
-	printf("输入顶点数 G.vexnum: ");scanf("%d",&G.vexnum);
-	printf("输入边数 G.arcnum: ");scanf("%d",&G.arcnum);
-	getchar();//吸收缓冲区的回车
-	for(i=0;i<G.vexnum;++i){
-		printf("输入顶点G.vexs[%d]: ",i);
-		scanf("%c",&G.vexs[i]);getchar();
-	}
-	for(i=0;i<G.vexnum;++i){
-		for(j=0;j<G.vexnum;++j){
-			G.arcs[i][j].adj=INFINITY;
-			G.arcs[i][j].info=nullptr;
-		}
-	}
-	for(k=0;k<G.arcnum;++k){
-		printf("输入第 %d 条边的顶点 vi、vj 以及权值 w (int):\n",k+1);
-		scanf("%c %c %d",&v1,&v2,&w);getchar();
-		i=LocateVex(G,v1);j=LocateVex(G,v2);
-		G.arcs[i][j].adj=G.arcs[j][i].adj=w;//无向网 
-		//if(Inclnfo)scanf(G.arcs[i][j].info)//输入弧含有的相关信息 
-	}
-	return Ok; 
+	S.base=(SElemType*)malloc(sizeof(SElemType)*STACK_INIT_SIZE);
+	if(!S.base)exit(Overflow);
+	S.top=S.base;
+	S.stacksize=STACK_INIT_SIZE;
+	return Ok;
 }
 
-Status CreateDG(MGraph &G)
+Status Pop(SqStack &S,SElemType &e)
 {
-	int i,j,k;
-	VertexType v1,v2;
-	printf("输入顶点数 G.vexnum: ");scanf("%d",&G.vexnum);
-	printf("输入弧数 G.arcnum: ");scanf("%d",&G.arcnum);
-	getchar();//吸收缓冲区的回车
-	for(i=0;i<G.vexnum;++i){
-		printf("输入顶点G.vexs[%d]：",i);
-		scanf("%c",&G.vexs[i]);getchar();
+	if(S.base==S.top)return Error;
+	S.top--;
+	e=*S.top;
+	return Ok;
+}
+
+Status Push(SqStack &S,SElemType e)
+{
+	if(S.top-S.base>=S.stacksize){
+		S.base=(SElemType*)realloc(S.base,sizeof(SElemType)*(S.stacksize+STACKINCREMENT));
+		if(!S.base)exit(Overflow);
+		S.top=S.base+S.stacksize;
+		S.stacksize+=STACKINCREMENT;
 	}
-	for(i=0;i<G.vexnum;++i){
-		for(j=0;j<G.vexnum;++j){
-		//	G.arcs[i][j]=0; 仔细！arcs是什么数据结构！
-					//arcs[i][j]还只是指向了某一个大的结构体
-					//还没有具体指向结构体中的哪一个细分结构！
-			G.arcs[i][j].adj=0;
-			G.arcs[i][j].info=nullptr;			 
+	*S.top=e;
+	S.top++;
+	return Ok;
+}
+
+Status GetTop(SqStack S,SElemType &e)
+{
+	if(S.base==S.top)return Error;
+	e=S.top[-1];
+	return Ok;
+}
+
+Status SearchBST(BiTree T,KeyType key,BiTree f,BiTree &p)
+{
+	if(!T){
+		p=f;return False;
+	}
+	else if(T->data==key){
+		p=f;return Ok;
+	}
+	else if(key<T->data){
+		return SearchBST(T->lchild,key,T,p);
+	}
+	else{
+		return  SearchBST(T->rchild,key,T,p);
+	}
+}
+
+Status PreOrderTraverse(BiTree T)
+{
+	SqStack(S);InitStack(S);
+	while(T||!StackEmpty(S)){
+		while(T){
+			printf("%d",T->data);
+			Push(S,T);
+			T=T->lchild;
+		}
+		if(!StackEmpty(S)){
+			Pop(S,T);
+			T=T->rchild;
 		}
 	}
-	for(k=0;k<G.arcnum;++k){
-	//	printf("输入弧尾v1，弧头v2：",);scanf("%c %c",&v1,&v2);
-		printf("输入第 %d 条弧的弧尾 vi，弧头vj ：\n",k+1);
-		scanf("%c %c",&v1,&v2);getchar();
-		i=LocateVex(G,v1);j=LocateVex(G,v2);
-		G.arcs[i][j].adj=1;//有向图 
+	return Ok;
+
+}
+
+Status InOrderTraverse(BiTree T)
+{
+	SqStack(S);InitStack(S);
+	while(T||!StackEmpty(S)){
+		while(T){
+			Push(S,T);
+			T=T->lchild;
+		}
+		if(!StackEmpty(S)){
+			Pop(S,T);
+			printf("%d",T->data);
+			T=T->rchild;
+		}
 	}
 	return Ok;
 }
 
-Status CreateUDG(MGraph &G)
+Status PostOrderTraverse(BiTree T)
 {
-	int i,j,k;
-	VertexType v1,v2;
-	printf("输入顶点数 G.vexnum：");scanf("%d",&G.vexnum);
-	printf("输入边数 G.arcnum：");scanf("%d",&G.arcnum);
-	//怎么刚说这里 又错了！。。。。
-//
-	getchar(); 
-	//仔细啊啊啊啊啊啊！！ 
-	for(i=0;i<G.vexnum;++i){
-		printf("输入顶点 G.vexs[%d]：",i);
-		scanf("%c",&G.vexs[i]);getchar();
-	}
-	for(i=0;i<G.vexnum;++i){
-		for(j=0;j<G.vexnum;++j){
-			G.arcs[i][j].adj=0;
-			G.arcs[i][j].info=nullptr;
+	SqStack S;InitStack(S);
+	Push(S,T);
+	BiTree pre=nullptr; 
+	while(!StackEmpty(S)){
+		GetTop(S,T);
+		if(!T->lchild&&!T->rchild||pre&&(pre==T->lchild||pre==T->rchild)){
+			Pop(S,T);printf("%d",T->data);pre=T;
 		}
-	}
-	for(k=0;k<G.arcnum;++k){
-		printf("输入第 %d 条边的顶点 v1、v2：\n",k+1);
-		scanf("%c %c",&v1,&v2);getchar();
-		i=LocateVex(G,v1);j=LocateVex(G,v2);
-		G.arcs[i][j].adj=G.arcs[j][i].adj=1;
-	}
-	return Ok;
-}
-
-Status CreateDN(MGraph &G)
-{
-	int i,j,k,w;
-	VertexType v1,v2;
-	printf("输入顶点个数 G.vexnum：");scanf("%d",&G.vexnum);
-	printf("输入弧个数 G.arcnum：");scanf("%d",&G.arcnum);
-	///////
-//	这里一定要仔细！！！ 
-	///////
-//	上一个数据输入的是%d 而下一个数据输入的是%c ！
-//	中间一定要有将缓冲区的回车符刷新掉（吸收掉）的操作！ 
-	getchar();
-	///////
-	for(i=0;i<G.vexnum;++i){
-		printf("输入顶点G.vexs[%d]：",i);
-		scanf("%c",&G.vexs[i]);getchar();
-	}
-	for(i=0;i<G.vexnum;++i){
-		for(j=0;j<G.vexnum;++j){
-			G.arcs[i][j].adj=INFINITY;
-			G.arcs[i][j].info=nullptr;
-		}
-	}
-	for(k=0;k<G.arcnum;++k){
-		printf("输入第 %d 条弧的弧尾 vi，弧头vj 以及权值 w (int)：\n",k+1);
-		scanf("%c %c %d",&v1,&v2,&w);getchar(); 
-		i=LocateVex(G,v1);j=LocateVex(G,v2);
-		G.arcs[i][j].adj=w;
-	}
-	return Ok;
-}
-
-Status CreateGraph(MGraph &G)
-{
-	//采用数组（邻接矩阵）表示法，构造图G
-	printf("请输入图的种类: 0 表示 DG, 1 表示 DN, 2 表示 UDG , 3 表示 UDN\n");
-	scanf("%d",&G.kind);
-	switch(G.kind){
-		case DG:return CreateDG(G);
-		case DN:return CreateDN(G);
-		case UDG:return CreateUDG(G);
-		case UDN:return CreateUDN(G);
-		default:return Error;
-	}
-	return Ok;
-}
-
-void list(MGraph G)
-{
-	int i,j;
-	printf("输出邻接矩阵：\n\n");
-	printf(" ----");
-	for(i=0;i<G.vexnum;++i){
-		printf("%4c",G.vexs[i]);
-	}
-	printf("\n");
-	for(i=0;i<G.vexnum;++i){
-		printf("%c----",G.vexs[i]);
-		for(j=0;j<G.vexnum;++j){
-			if(G.arcs[i][j].adj==INFINITY)
-				printf("%4s","∞");
-			else
-				printf("%4d",G.arcs[i][j].adj);
-		}
-		printf("\n");
-	}
-}
-
-typedef struct mini{
-	VertexType adjvex;//理解这个辅助数组的数据结构！
-					//这个adjvex 代表的是弧尾的值了！ 
-	VRType lowcost;
-}closedge[MAX_VERTEX_NUM];
-
-Status MiniSpanTree_PRIM(MGraph G,VertexType u)
-{
-	int k=LocateVex(G,u);closedge Aid;
-	for(int i=0;i<G.vexnum;++i){
-		if(i!=k){
-			Aid[i].lowcost=G.arcs[k][i].adj;
-			Aid[i].adjvex=G.vexs[k];
-		}
-	}
-	Aid[k].lowcost=0;Aid[k].adjvex=u;
-//	cout<<endl;
-//	for(int i=0;i<G.vexnum;++i)if(Aid[i].lowcost!=INFINITY)cout<<Aid[i].lowcost;else cout<<"s"; 
-	for(int i=1;i<G.vexnum;++i){
-		int min=INFINITY;
-		for(int j=0;j<G.vexnum;++j){//找出最小的邻边！ 
-		//	int min=INFINITY;
-			//啊啊啊啊上一行真是傻了。。。每一次新循环都会重置啊！仔细。。 
-			if(Aid[j].lowcost<min&&Aid[j].lowcost!=0){
-				min=Aid[j].lowcost;k=j;
-	//			cout<<"k="<<k; 
-			}
-		}
-	//	printf("now k=%d\n",k);
-		Aid[k].lowcost=0;
-		printf("%c%c ",G.vexs[k],Aid[k].adjvex);
-		for(int i=0;i<G.vexnum;++i){
-			if(Aid[i].lowcost>G.arcs[k][i].adj){
-				Aid[i].lowcost=G.arcs[k][i].adj;
-				Aid[i].adjvex=G.vexs[k];
-			}
+		else{
+			if(T->rchild)Push(S,T->rchild);
+			if(T->lchild)Push(S,T->lchild);
 		}
 	}
 	return Ok;
-} 
+ }
 
-int main()
+Status InsertBST(BiTree &T,ElemType e)
 {
-	MGraph G;
-	CreateGraph(G);
-	list(G);
-	printf("\n");
-	printf("输出最短：\n");
-	MiniSpanTree_PRIM(G,'a');
-	printf("\n");
+	BiTree p;
+	if(!SearchBST(T,e.key,nullptr,p)){
+		BiTree s=(BiTNode*)malloc(sizeof(BiTNode));
+		s->data=e.key;s->lchild=nullptr;s->rchild=nullptr;
+		//注意一开始！最开始是空树的情况。。此时p=nullptr!
+		if(!p)T=s;
+		else if(e.key<p->data)p->lchild=s;//
+		else p->rchild=s;
+		return Ok;
+	}
+	return False;
+}
+
+int main ()
+{
+	BiTree T=nullptr,s;
+	int n,i;
+	ElemType e;
+	printf("输入二叉排序树的结点数：");
+	scanf("%d",&n);
+	printf("输入%d个结点的值：\n",n);
+	for(i=1;i<=n;++i){
+		scanf("%d",&e.key);
+		InsertBST(T,e);
+	}
+	printf("输出二叉排序树的先序遍历结果：\n");
+	PreOrderTraverse(T);
+	cout<<endl;
+	printf("输出二叉排序树的中序遍历结果：\n");
+	InOrderTraverse(T);
+	cout<<endl;
+	printf("输出二叉排序树的后序遍历结果：\n");
+	PostOrderTraverse(T);
+	cout<<endl;
+	printf("输入查找结点的值：\n");
+	scanf("%d",&e.key);
+	bool flag=SearchBST(T,e.key,nullptr,s);
+	if(flag)printf("已找到！结点的八进制地址为：%o\n",&s);
+	else printf("未找到！\n");
+	printf("输入插入结点的值：\n",n);
+	scanf("%d",&e.key);
+	InsertBST(T,e);
+	printf("输出二叉排序树的先序遍历结果：\n");
+	PreOrderTraverse(T);
+	cout<<endl;
+	printf("输出二叉排序树的中序遍历结果：\n");
+	InOrderTraverse(T);
+	cout<<endl;
+	printf("输出二叉排序树的后序遍历结果：\n");
+	PostOrderTraverse(T);
+	cout<<endl;
+	printf("输入删除结点的值：\n");
+	scanf("%d",&e.key);
+	DeleteBST(T,e.key);
+	printf("输出二叉排序树的先序遍历结果：\n");
+	PreOrderTraverse(T);
+	cout<<endl;
+	printf("输出二叉排序树的中序遍历结果：\n");
+	InOrderTraverse(T);
+	cout<<endl;
+	printf("输出二叉排序树的后序遍历结果：\n");
+	PostOrderTraverse(T);
+	cout<<endl;
 	return 0;
-}
-
-
-
-
-
-
-
-
-
+} 
 
 
