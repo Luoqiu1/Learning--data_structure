0d1c770b4e263e1024ad7eaf3f3e7575358930a1
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-24 00:28:22 +0800

    初探待排数据结构类型

---
+++ 16ed/初探待排记录数据类型.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define MAXSIZE 20
+typedef int KeyType;
+typedef char InfoType 
+typedef struct{
+	KeyType key;
+	InfoType *otherInfo;
+//};
+}RedType;
+typedef struct {
+//
+	RedType r[MAXSIZE+1];	
+//
+	int length;
+}SqList; 
+
+int main ()
+{
+	return 0;
+}
