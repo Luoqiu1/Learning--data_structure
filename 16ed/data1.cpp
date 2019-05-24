2719b5148ac33b624c8aa640f1a547f1fd8699de
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-24 10:23:24 +0800

    对关键字域及记录类型的新理解

--- 16ed/data1.cpp
+++ 16ed/data1.cpp
@@ -20,13 +20,25 @@ Status InsertSort(SqList &L)
 {
 	for(int i=2;i<=L.length;++i){
 		if(L.r[i].key<L.r[i-1].key){
-			L.r[0].key=L.r[i].key;
-			L.r[i].key=L.r[i-1].key;
+		//	L.r[0].key=L.r[i].key;
+		//	L.r[i].key=L.r[i-1].key;      //这些注释的几行，没有弄清楚交换的条件以及实质！
+									//if条件中比较的是关键字大小，通过关键字来排序数据！
+									//而不是只是去交换关键字数据！
+									//是通过关键字，将整块数据域交换！
+					
+			L.r[0]=L.r[i];
+			L.r[i]=L.r[i-1];		//这样才对！ 
+					 
 			int j;
-			for(j=i-2;L.r[j].key<L.r[0].key;--j){
-				L.r[j+1].key=L.r[j].key;
+		//	for(j=i-2;L.r[j].key<L.r[0].key;--j){
+								//要保证稳定性！相等时，后面的数据仍在后面！
+			for(j=i-2;L.r[j].key<=L.r[0].key;--j){
+		//		L.r[j+1].key=L.r[j].key;
+				L.r[j+1]=L.r[j];
 			}
-			L.r[j+1].key=L.r[0].key;
+		//	L.r[j+1].key=L.r[0].key;
+			L.r[j+1]=L.r[0];
+		
 		}
 	}
 	return Ok;
