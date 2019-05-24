0aa5fafa579e8429fe292bf4cdc396365d6b5867
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-05-24 10:59:08 +0800

    直接插入排序完成！要仔细理解条件，别弄错了！

--- 16ed/data1.cpp
+++ 16ed/data1.cpp
@@ -9,16 +9,43 @@ typedef int KeyType;
 typedef char InfoType;
 typedef struct{
 	KeyType key;
-	InfoType *otherInfo;
+	InfoType *otherInfo=nullptr;
 }RedType;
 typedef struct{
 	RedType r[MAXSIZE+1];
 	int length;
 }SqList;
 
+Status InitList(SqList &L)
+{
+	L.length=0;
+	return Ok;
+}
+
+Status CreateList(SqList &L,int n)
+{
+	printf("输入%d个元素：\n",n); 
+	for(int i=1;i<=n;++i){
+		scanf("%d",&L.r[i].key);
+		++L.length;
+	}
+	if(L.length==0)return Error;
+	return Ok;
+}
+
+Status DispList(SqList L)
+{
+	if(L.length==0)return Error;
+	for(int i=1;i<=L.length;++i){
+		printf("%d ",L.r[i].key);
+	}
+	printf("\n");
+	return Ok;
+}
+
 Status InsertSort(SqList &L)
 {
-	for(int i=2;i<=L.length;++i){
+	for(int i=2;i<=L.length;++i){ 
 		if(L.r[i].key<L.r[i-1].key){
 		//	L.r[0].key=L.r[i].key;
 		//	L.r[i].key=L.r[i-1].key;      //这些注释的几行，没有弄清楚交换的条件以及实质！
@@ -32,7 +59,13 @@ Status InsertSort(SqList &L)
 			int j;
 		//	for(j=i-2;L.r[j].key<L.r[0].key;--j){
 								//要保证稳定性！相等时，后面的数据仍在后面！
-			for(j=i-2;L.r[j].key<=L.r[0].key;--j){
+	//		for(j=i-2;L.r[j].key<=L.r[0].key;--j){//元素后移一位操作 
+	
+	//		for(j=i-2;j>=0&&L.r[j].key<=L.r[0].key;--j){
+			for(j=i-2;j>=0&&L.r[0].key<L.r[j].key;--j){//元素后移一位操作
+					//仔细啊！ 是若哨兵单元还小于这个数 就往下走！  前面的条件全错了！！！一定要仔细。。。。 
+							//已经等于时也要跳出！为了保证稳定性！ 
+													//还需要保证 j>=0!数组访问不越界！ 
 		//		L.r[j+1].key=L.r[j].key;
 				L.r[j+1]=L.r[j];
 			}
@@ -46,5 +79,18 @@ Status InsertSort(SqList &L)
 
 int main ()
 {
+	SqList L;
+	int n;
+	printf("初始化顺序表，");
+	InitList(L);
+	if(L.length==0)printf("顺序表为空！\n");
+	printf("输入顺序表的元素个数：");
+	scanf("%d",&n);
+	CreateList(L,n);
+	printf("输出%d个元素的顺序表如下：\n",n);
+	DispList(L);
+	InsertSort(L);
+	printf("直接插入排序结果如下：\n");
+	DispList(L); 
 	return 0;
 }
