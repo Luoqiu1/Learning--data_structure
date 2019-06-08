8e3d4790f559398345f31dd62d181b886b041390
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-06-08 16:10:54 +0800

    插入操作回顾！

--- 期末复习重新实现/实验1 顺序表的实现和应用/data2.cpp
+++ 期末复习重新实现/实验1 顺序表的实现和应用/data2.cpp
@@ -17,3 +17,30 @@ Status InitList_Sq(SqList &L)
 	L.length=0;
 	return OK;
  } 
+ 
+Status ListInsert_Sq(SqList &L,int i,ElemType e)
+{
+	if(i<1||i>L.length+1)return Error;
+	if(!L.length)return Error;
+	if(L.length+1>L.listsize){
+//	该条件与 L.length>=L.listsize等效！
+//	因为每次只插入一个元素！	
+		L.elem=(ElemType*)realloc(L.elem,(L.listsize+LISTINCREMENT)*sizeof(ElemType));
+		if(!L.elem)exit(Overflow);
+	//	++L.listsize; 
+		//在想啥。。。每次增加空间不止是只增加一个啊！
+		L.listsize+=LISTINCREMENT;
+		 
+	}
+	for(int j=L.length-1;j>=i-1;--j){
+	//	L.data[j+1]=L.data[j];
+		//仔细。。结构体里的类型名是elem！不是data。。！
+		L.elem[j+1]=L.elem[j];
+	}
+	L.elem[i-1]=e;
+	//怎么又忘记长度要加一了！！！
+	++L.length;
+	 
+	return Ok;
+}
+
