cb660fb3d8739fed16097f4b03d5be1d14bc1a96
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-06-10 23:40:32 +0800

    8ed 作业二 回顾！我的做法是不止适用于递增单链表了！

--- 期末复习重新实现/实验8 二叉树的创建和遍历/data1.cpp
+++ 期末复习重新实现/实验8 二叉树的创建和遍历/data1.cpp
@@ -8,7 +8,14 @@ Status Go(LinkList &L)
 {
 	LinkList p=L,q=p;
 	while(p){
-		while(q->next->data>=0)q=q->next;
+	//	while(q->next->data>=0)q=q->next;
+		//在访问指针前，先判断指针是否为空啊！。
+		while(q->next&&q->next->data>=0)q=q->next;
+		
+		//考虑到，当没有找到小于0的结点后，即可结束循环！
+		//找到的话，则执行以下语句
+		if(!q->next)break; 
+		
 		LinkList r=q->next;
 		q->next=r->next;
 	//	r->next=L->next;
@@ -24,5 +31,27 @@ Status Go(LinkList &L)
 		p=p->next;
 		q=p;
 	}
-	
 } 
+
+Status Go2(LinkList &L)
+{
+	LinkList p=L->next,q=p;
+	while(p){
+		ElemType judge=p->data;
+		while(q){
+			while(q->next&&q->next->data!=judge)q=q->next;
+			if(!q->next)break;
+			LinkList r=q->next;
+			q->next=r->next;
+			free(r);
+			q=p;
+		}
+		p=p->next;
+		q=p;//这里别忘记更新啊！。。 
+	}
+}
+
+
+
+
+
