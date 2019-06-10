953f6f93447e2d66886ecb8d1612adff7ed0f6f8
Luoqiu1 <49295370+Luoqiu1@users.noreply.github.com>
2019-06-11 00:05:55 +0800

    回顾 后序递归二叉树

--- 期末复习重新实现/实验8 二叉树的创建和遍历/递归创建二叉树.cpp
+++ 期末复习重新实现/实验8 二叉树的创建和遍历/递归创建二叉树.cpp
@@ -3,7 +3,7 @@
 Status PreOrderTraverse(BiTree T)
 {
 	if(T){
-		printf("%d",T->data);
+		printf("%c",T->data);
 //		PreOrderTraverse(T->lchild);
 //		PreOrderTraverse(T->rchild);
 		//不够给力。。既然用到了返回状态的Status，最好利用起来！
@@ -14,4 +14,40 @@ Status PreOrderTraverse(BiTree T)
 	return Ok;
  }
  
-Status InOrderTraverse()
+Status InOrderTraverse(BiTree T)
+{
+//	if(T){
+//		if(InOrderTraverse(T->lchild)){
+//			printf("%c",T->data);
+//			if(InOrderTraverse(T->rchild))return Ok;
+//		}
+//	}
+//	return Ok;
+
+	//写法不够好
+	int m=0;
+	if(T){
+		if(InOrderTraverse(T->lchild))m=1;
+		printf("%c",T->data);
+		if(m)
+			if(InOrderTraverse(T->rchild))return Ok;
+			return Error;
+	}
+	return Ok;
+	
+}
+
+Status PostOrderTraverse(BiTree T)
+{
+	if(T){
+		if(PostOrderTraverse(T->lchild))
+			if(PostOrderTraverse(T->rchild)){
+				printf("%c",T->data);return Ok;
+			}
+		return Error;
+	}
+	return OK;
+}
+
+
+
